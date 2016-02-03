ac298eff541a611803b6e23c69f24e0d09e85d5d
shumana <shumana@nurs-iMac.local>
2016-02-03 16:45:19 +0600

    sdf

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ var config = require('./config'),
     mongoose = require('mongoose');
 
 module.exports = function () {
-    var db = mongoose.connect(config.db);
+    //var db = mongoose.connect(config.db);
     require('../app/models/smile.server.model');
 
 
