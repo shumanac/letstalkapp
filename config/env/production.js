09a9c42283734abee2d01a83a0ce338cf1126550
shumana <shumana@nurs-iMac.local>
2016-02-03 17:01:23 +0600

    db addd

--- config/env/production.js
+++ config/env/production.js
@@ -1,4 +1,4 @@
 module.exports = {
     port: process.env.PORT,
-    db: 'mongodb://localhost/letstalk'
+    db: 'mongodb://shumana:Shumanarocks1@ds055565.mongolab.com:55565/letstalk'
 };
--- config/mongoose.js
+++ config/mongoose.js
@@ -2,9 +2,9 @@ var config = require('./config'),
     mongoose = require('mongoose');
 
 module.exports = function () {
-    //var db = mongoose.connect(config.db);
+    var db = mongoose.connect(config.db);
     require('../app/models/smile.server.model');
 
 
-    //return db;
+    return db;
 };
